31eb8ee77e264dfce860b21bd1b09d9242841bc0
卓訓發 <frank.cho@cypress.com.tw>
2020-06-11 11:05:10 +0800

    [update] improve empty component usage

--- build/empty/empty.d.ts
+++ build/empty/empty.d.ts
@@ -3,6 +3,7 @@ declare type EmptyProps = {
     className: string;
     style: React.CSSProperties;
     text: string;
+    textColor: string;
 };
 export default class Empty extends React.Component<Partial<EmptyProps>> {
     render(): JSX.Element;
