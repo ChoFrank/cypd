78524b2f43e0d3adff56c9f620a08c101a398ec9
卓訓發 <frank.cho@cypress.com.tw>
2020-08-24 13:20:05 +0800

    [fixed] try fix tooltip delete error

--- build/index.js
+++ build/index.js
@@ -30307,7 +30307,7 @@ var Tooltip = /** @class */ (function (_super) {
             var container = document.getElementById(_this.tooltipId);
             if (container) {
                 container.classList.add('hide');
-                setTimeout(function () { if (container)
+                setTimeout(function () { if (container && document.getElementById(container.id))
                     document.body.removeChild(container); }, 150);
                 // setTimeout(() => { if (container) container.remove(); }, 150);
             }
